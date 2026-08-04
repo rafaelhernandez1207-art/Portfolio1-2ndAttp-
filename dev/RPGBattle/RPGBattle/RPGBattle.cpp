96bd8af7e6c24f468c92ed515766619539b8f850
rafaelhernandez1207-art <rafaelhernandez1207@gmail.com>
2026-08-04 10:43:55 -0700

    initial files added/ testing if changes show in github

--- dev/RPGBattle/RPGBattle/RPGBattle.cpp
+++ dev/RPGBattle/RPGBattle/RPGBattle.cpp
@@ -1,11 +1,11 @@
-// FirstApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// RPGBattle.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!!\n";
+    std::cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
